34e34ae3fb328eb3734e6b87e0a26ae91a3cec13
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-01-22 23:55:12 +0530

    New learning

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+// AddTwoNUmber --> Pascal naming convention
+// addTwoNumber --> Camel case naming convention
