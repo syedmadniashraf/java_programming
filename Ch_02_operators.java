25463d5b4490aa6d08bc7acfdd43d5439b13d448
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-01-30 22:55:15 +0530

    operators

--- Ch_02_operators.java
+++ Ch_02_operators.java
@@ -1,7 +1,7 @@
 public class Ch_02_operators {
     public static void main(String[] args) {
         int a = 4;
-        int b = 6 - a;
+        int b = 6 * a;
         System.out.println(b);
     }
 }
