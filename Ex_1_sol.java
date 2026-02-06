65622685f1797e9c6f9f3169bc294908110d7b3a
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-06 22:15:51 +0530

    Exercise_1 Solution

---
+++ Ex_1_sol.java
@@ -0,0 +1,23 @@
+// Calculate the percentage of 5 subjects
+
+import java.util.Scanner;
+public class Ex_1_sol {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your physics marks:");
+        int Physics = scan.nextInt();
+        System.out.println("Enter your Chemistry marks:");
+        int Chemistry = scan.nextInt();
+        System.out.println("Enter your Maths marks:");
+        int Maths = scan.nextInt();
+        System.out.println("Enter your English marks:");
+        int English = scan.nextInt();
+        System.out.println("Enter your Computer Science marks:");
+        int Computer_Science  = scan.nextInt();
+
+        float Percentage = ((Physics + Chemistry + Maths + English + Computer_Science)/500.0f)*100;
+        
+        System.out.println("Percentage: ");
+        System.out.println(Percentage);
+    }
+}
