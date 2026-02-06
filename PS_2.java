dee7520069d1532ee8a11ba9eebb0f511bfb09cc
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-06 22:31:25 +0530

    PS_2 Completed

---
+++ PS_2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PS_2 {
+    public static void main(String[] args) {
+        
+        // Ques 1
+        // float a = 7/4.0f * 9/2.0f;
+        // System.out.println(a);
+
+        // Ques 2
+        // char grade = 'B';
+        // grade = (char)(grade + 8);
+        // System.out.println(grade);
+
+        // // Decrypting grade
+        // grade = (char)(grade - 8);
+        // System.out.println(grade);
+
+        // Ques 3
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter number: ");
+        // int a = sc.nextInt();
+        // System.out.println(a > 8);
+
+    }
+}
