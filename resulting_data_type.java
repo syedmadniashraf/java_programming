69aff3c0636bc9868aa048e5809ea5f539359cae
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-06 19:25:11 +0530

    resulting_data_type

---
+++ resulting_data_type.java
@@ -0,0 +1,31 @@
+public class resulting_data_type {
+    public static void main(String[] args) {
+        /*
+        byte x = 5;
+        int y = 6;
+        short z = 4;
+        int a = y + z;
+        float b = 6.54f + x;
+        System.out.println(b);
+        */
+
+        // Increment & Decrement operator
+        /* 
+        int i = 56;
+        System.out.println(i++); // First i assigned to 56 then incremented (56)
+        System.out.println(i); // 57
+        System.out.println(++i); // First i incremented then assigned to i (58)
+        System.out.println(i); // 58
+        */ 
+
+        // Quick Quiz
+
+        int y = 7;
+        int x = ++y * 8;
+        System.out.println(x);
+
+        char ch = 'a';
+        System.out.println(++ch); // 'b'
+
+    }
+}
