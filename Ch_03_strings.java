f447a0e20b789a84b2b3dd556b17231da2b2c574
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-09 23:58:07 +0530

    Ch_03_strings

---
+++ Ch_03_strings.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Ch_03_strings {
+    public static void main(String[] args) {
+        //String name = new String("Harry");
+        // String name = "Harry";
+        // System.out.println(name);
+
+        // int a = 6;
+        // float b = 5.645f;
+        // // System.out.printf("The value of a is %d and value of b is %f",a, b);
+        // System.out.format("The value of a is %d and value of b is %f",a, b);
+
+        Scanner sc = new Scanner(System.in);
+        // String st = sc.next();
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
