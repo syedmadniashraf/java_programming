8074be5d5a5a4c0da36d8b48f3f3b15b6c13e966
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-10 23:37:43 +0530

    string_methods

---
+++ string_methods.java
@@ -0,0 +1,38 @@
+public class string_methods {
+    public static void main(String[] args) {
+        String name = "Harry";
+        // System.out.println(name);
+        // int value = name.length();
+        // System.out.println(value);
+
+        // String lString = name.toLowerCase();
+        // System.out.println(lString);
+
+        // String uString = name.toUpperCase();
+        // System.out.println(uString);
+
+        // String nonTrimmedString = "    Harry     ";
+        // System.out.println(nonTrimmedString.trim());
+        
+        // System.out.println(name.substring(2));  // Print name start from index 2 to end.
+
+        // System.out.println(name.substring(1,4));  // Start index included end index excluded.
+
+        // System.out.println(name.replace('r', 'p')); // Replace 'r' to 'p'
+        // System.out.println(name.replace("rry", "ier")); // Replace 'r' to 'p'
+
+        // System.out.println(name.startsWith("Ha"));
+        // System.out.println(name.endsWith("Ha"));
+
+        // System.out.println(name.charAt(2)); // Returns character at given index
+
+        // System.out.println(name.indexOf("y",3));
+        // System.out.println(name.lastIndexOf("r",2));
+
+        // System.out.println(name.equals("Harry"));
+        // System.out.println(name.equalsIgnoreCase("harry"));
+
+        // System.out.println("I am escape sequence \\ \" Double quote");
+    }
+
+}
