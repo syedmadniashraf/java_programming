461d5f2a73db959ed9d03e75dfd2174f914f5b9f
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-19 12:38:06 +0530

    string_ps

---
+++ string_ps.java
@@ -0,0 +1,33 @@
+public class string_ps {
+    public static void main(String[] args) {
+        // question 1:
+        // String name = "Jack";
+        // name = name.toLowerCase();
+        // System.out.println(name);
+
+        // question 2:
+
+        // String name = "I am good boy";
+        // System.out.println(name.replace(" ", "_"));
+
+        // question 3:
+
+        // String text = "Dear <|name|>, Thanks a lot!";
+        // text = text.replace("<|name|>", "Syed Madni");
+        // System.out.println(text);
+
+        // question 4:
+
+        // String text = "My name   is Syed Madni";
+        // System.out.println(text.indexOf("  "));
+        // System.out.println(text.indexOf("   "));
+        
+
+        // question 5:
+
+        // String letter = "Dear Harry,\nThis java course is nice.\nhanks";
+        // System.out.println(letter);
+        
+    }
+
+}
