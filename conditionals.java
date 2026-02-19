a1168ab456493910a412e5df0b7ece34c0c7fbd8
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-02-19 13:17:37 +0530

    conditionals

---
+++ conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int age = 18;
+        // boolean cond = (age == 18);
+        if (age>18) {
+            System.out.println("Yes you can drive");
+        }
+        else{
+            System.out.println("No you cannot drive");
+        }
+    }
+}
