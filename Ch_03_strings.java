ca8c58ed3c2b1dbe1346b451a18830fd7a3d3743
Syed Madni Ashraf <syedmadniashraf.offc@gmail.com>
2026-03-13 23:49:13 +0530

    Ch_03_strings

--- Ch_03_strings.java
+++ Ch_03_strings.java
@@ -5,7 +5,7 @@ public class Ch_03_strings {
         // String name = "Harry";
         // System.out.println(name);
 
-        int a = 6;
+        int a = 4;
         float b = 5.645f;
         // System.out.printf("The value of a is %d and value of b is %f",a, b);
         System.out.format("The value of a is %d and value of b is %f",a, b);
